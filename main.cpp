8387da4ea088efa8509a5f1d006e2ddedb660b20
Jens Coenders <jens@famcoenders.nl>
2023-01-03 11:59:32 +0100

    Add code to read file contents from model-checker project

--- main.cpp
+++ main.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+
+bool readFileContents(const char *filename, string &out) {
+    // Open filename stream
+    ifstream in;
+    in.open(filename);
+
+    // Check if filename exists
+    if (!in.is_open()) {
+        return false;
+    }
+
+    // Read filename contents
+    stringstream stream;
+    stream << in.rdbuf();
+    out = stream.str();
+
+    // Close input stream
+    in.close();
+
+    return true;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
